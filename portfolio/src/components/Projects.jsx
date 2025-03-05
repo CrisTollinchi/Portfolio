ceed8ebb0555b91d0d3b254add16049d3dfc0d79
CrisTollinchi <cristollinchi@gmail>
2025-03-05 17:01:54 -0500

    Updated Projects Component with place holdres

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,9 +1,15 @@
-
+import Title from "./Title"
 
 const Projects = () => {
     return(
-        <div>
-            <h1>Projects</h1>
+        <div className="p-2">
+           <Title titleContent="Projects"/>
+           <div className="flex flex-wrap gap-1 m-2">
+            <div className="w-24 h-24 bg-amber-400">Project 1</div>
+            <div className="w-24 h-24 bg-red-500">Project 2</div>
+            <div className="w-24 h-24 bg-blue-400">Project 3</div>
+            <div className="w-24 h-24 bg-purple-400">Project 4</div>
+           </div>
         </div>
     )
 }
